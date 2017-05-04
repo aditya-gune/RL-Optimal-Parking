3fe4cbb3444ca0ddcfa729e6a362911dcdf0d12e
agune001 <ca_adi@yahoo.com>
2017-05-04 13:55:37 -0700

    simulator skeleton
    Skeleton for the simulator

--- simulator.py
+++ simulator.py
@@ -4,6 +4,7 @@ Created on Thu May 04 11:24:13 2017
 
 @author: Aditya
 """
+import numpy as np
 
 class Simulator:
     def __init__(self, mdp):
@@ -15,15 +16,39 @@ class Simulator:
         self.states_visited = []
         self.terminal = False
         
-    def runPolicy(self, mdp, policy):
-        (states, actions, R, T) = mdp.get_MDP()
+        
+    def runPolicy(self, policy):
+        (states, actions, R, T) = self.mdp.get_MDP()
         while not self.terminal:
             current = self.current
             self.states_visited.append(current)
             self.R_tot += R[current]
-            take_action(policy[current])
+            self.take_action(policy[current])
             self.actions_taken.append(policy[current])
         return (self.R_tot, self.actions_taken, self.states_visited)
     
-    
+    def take_action(self, action):
+        if self.terminal:return
+        (states, actions, R, T) = self.mdp.get_MDP()
+        
+        tarray = np.array(states)
+        tvals = T[self.current][action]
+        
+        if np.sum(tvals) > 0:
+            distribution = np.add.accumulate(tvals)
+            randnum = np.random.random_sample(1)
+            index = min(distribution, key=lambda x:abs(x-randnum))
+            s_next = tarray[index]
+            self.current = s_next
+        
+            self.terminal = True
+            for action in range(actions):
+                tvals = T[self.current][action]
+                if np.sum(tvals) > 0:
+                    self.terminal = False
+                    break
+            
+        
+        
+        
             
