46238b0a880f4264967d648fc4fac55a66e5f541
Aditya Gune <adi.vtca@gmail.com>
2017-05-04 23:46:01 -0700

    refactoring  sim i/o

--- main.py
+++ main.py
@@ -8,20 +8,21 @@ from mdp import MDP
 import sys
 from valueiter import *
 from parking import Parking_MDP
+from simulator import Simulator
 
 
+#Set up environment
 beta = 0.9
 epsilon = 0.00001
+numrows = 10
 
 mdp = MDP()
-
-numrows = 10
 
 #parking_mdp = Parking_MDP(rows=numrows, R_handicap=-100, R_coll=-10000)
 #parkingfile = open('ParkingMDP2.txt', 'w')
 #(states, actions, T, R) = parking_mdp.getMDP()
 #parking_mdp.writeMDP(parkingfile)
-
+(V, pi) = value_iter(states, actions, R, T, t, beta, epsilon)
 if len(sys.argv) > -1:
     k = mdp.read_MDP_from_file('ParkingMDP2.txt')
 else:
@@ -31,9 +32,15 @@ else:
 (states, actions, R, T) = mdp.process_MDP(k)
 
 
-t = open('output.txt', 'w+')
+#t = open('output.txt', 'w+')
 
-(V, pi) = value_iter(states, actions, R, T, t, beta, epsilon)
+simulator = Simulator((states, actions, R, T))
+simulator.runPolicy(pi)
+
+
+
+
+
 
 
 ##print("Optimal Value Function:")
@@ -49,4 +56,4 @@ t = open('output.txt', 'w+')
 #  #  print(x)
 #    t.write(str(x)+'\n')
 #parkingfile.close()
-t.close()
+#t.close()
--- simulator.py
+++ simulator.py
@@ -1,38 +1,38 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu May 04 11:24:13 2017
-
-@author: Aditya
+Simulator for Parking
 """
 import numpy as np
 
 class Simulator:
-    def __init__(self, mdp):
-        self.mdp = mdp
+    def __init__(self, mdp_Tuple:tuple):
         self.s_0 = 0
         self.current = self.s_0
         self.R_tot = 0
         self.actions_taken = []
         self.states_visited = []
         self.terminal = False
-        
+        self.states = mdp_Tuple[0]
+        self.actions = mdp_Tuple[1]
+        self.R = mdp_Tuple[2]
+        self.T = mdp_Tuple[3]        
         
     def runPolicy(self, policy):
-        (states, actions, R, T) = self.mdp.get_MDP()
+        #(states, actions, R, T) = self.get_MDP()
         while not self.terminal:
             current = self.current
             self.states_visited.append(current)
-            self.R_tot += R[current]
+            self.R_tot += float(self.R[current])
             self.take_action(policy[current])
             self.actions_taken.append(policy[current])
         return (self.R_tot, self.actions_taken, self.states_visited)
     
     def take_action(self, action):
         if self.terminal:return
-        (states, actions, R, T) = self.mdp.get_MDP()
+        #(states, actions, R, T) = self.get_MDP()
         
-        tarray = np.array(states)
-        tvals = T[self.current][action]
+        tarray = np.array(self.states)
+        tvals = self.T[self.current][action]
         
         if np.sum(tvals) > 0:
             distribution = np.add.accumulate(tvals)
@@ -42,12 +42,12 @@ class Simulator:
             self.current = s_next
         
             self.terminal = True
-            for action in range(actions):
-                tvals = T[self.current][action]
+            for action in range(self.actions):
+                tvals = self.T[self.current][action]
                 if np.sum(tvals) > 0:
                     self.terminal = False
                     break
-            
+            print('action terminal')
         
         
         
